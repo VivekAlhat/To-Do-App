bd6384804db99f6d369c0f7ac3637bd517262759
VivekAlhat <vivekalhat14@outlook.com>
2020-09-15 19:16:59 +0530

    Added Mongoose/Mongodb

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/src/date.js");
 const app = express();
 
-const notes = [];
+// Database setup
+mongoose.connect("mongodb://localhost:27017/todonotes", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const notesSchema = new mongoose.Schema({
+  noteItem: {
+    type: String,
+    required: true,
+  },
+});
+
+const Item = mongoose.model("Item", notesSchema);
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -13,15 +28,24 @@ app.listen(3000, function () {
 });
 
 app.get("/", function (request, response) {
-  response.render("todo", {
-    currDate: date.getDate(),
-    notes: notes,
-    count: notes.length,
+  Item.find(function (err, res) {
+    if (err) {
+      console.log(err);
+    } else {
+      response.render("todo", {
+        currDate: date.getDate(),
+        count: res.length,
+        notes: res,
+      });
+    }
   });
 });
 
 app.post("/", function (request, response) {
-  const task = request.body.task;
-  notes.push(task);
+  const t = request.body.task;
+  const note = new Item({
+    noteItem: t,
+  });
+  note.save();
   response.redirect("/");
 });
