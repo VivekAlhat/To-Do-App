c3e1fb6651d2bfb6529e7e6206a8f4bf2d07f04f
VivekAlhat <vivekalhat14@outlook.com>
2020-09-16 19:19:03 +0530

    Port Changed || Procfile Added

--- app.js
+++ app.js
@@ -36,7 +36,12 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.listen(3000, function () {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
   console.log("Server is up and running");
 });
 
